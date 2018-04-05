253be91e356cd023ff6d1858c556e983da6748a8
julia forster <juliaf@udel.edu>
2018-04-05 10:47:25 -0400

    added js examples

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,14 @@
+blist = ["end of the semseter", "no research", "disen gets better"];
+console.log(blist.lenth);
+blist.push("get a beer");
+
+console.log(blist.length);
+console.log(blist);
+
+/*for loop
+blbababa
+*/
+
+for (i=0; i<4; i++) {
+  console.log(blist[i]);
+}
