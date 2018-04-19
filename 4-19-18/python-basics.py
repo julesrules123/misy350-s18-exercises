9ac9ab8f73d1938ca914970f0638bdaba677ade3
julia forster <juliaf@udel.edu>
2018-04-19 10:23:48 -0400

    add exercises

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,9 @@
+#this is my first python code
+greeting = "hello"
+name = "Julia"
+print greeting # this print out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + " " + name + "!"
+print "%s %s!how are you doing today" % (greeting, name)
